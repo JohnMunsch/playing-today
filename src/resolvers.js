49cb995bf7563eb5089ea301012e00ea8dbbf2d8
John Munsch <john.munsch@gmail.com>
2019-01-21 15:25:59 -0600

    Mocks for mutation functions. Not yet tested.

--- src/resolvers.js
+++ src/resolvers.js
@@ -211,7 +211,80 @@ module.exports = {
   Query: {
     info: () =>
       `This is the API of a In/Out board for player check in for getting together to play games.`,
+    me: () => {
+      return players.find(
+        player => player.email === 'john.munsch@aptitude.com'
+      );
+    },
     games: () => games,
     players: () => players
+  },
+  Mutation: {
+    // (id: String!, playingToday: Boolean!): Player!
+    playing: (context, args) => {
+      // Find the player in question and set his/her status for gaming today.
+      let player = players.find(player => player.id === args.id);
+
+      player.playingToday = args.playingToday;
+
+      return player;
+    },
+
+    // (name: String!, notes: String, players: RecommendationInput): Game!
+    createGame: (context, args) => {
+      let newGame = {
+        // TODO: Add ID.
+        name: args.name,
+        notes: args.notes,
+        players: args.players
+      };
+
+      games.push(newGame);
+
+      return newGame;
+    },
+    // (id: String!): Game
+    deleteGame: (context, args) => {
+      let match = null;
+
+      games = games.filter(game => {
+        if (game.id === args.id) {
+          match = game;
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      return match;
+    },
+
+    // (email: String!): Player!
+    createPlayer: (context, args) => {
+      let newPlayer = {
+        // TODO: Add ID.
+        email: args.email,
+        playingToday: false
+      };
+
+      players.push(newPlayer);
+
+      return newPlayer;
+    },
+    // (id: String!): Player!
+    deletePlayer: (context, args) => {
+      let match = null;
+
+      players = players.filter(player => {
+        if (player.id === args.id) {
+          match = player;
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      return match;
+    }
   }
 };
