6a91be357f6b95a09f138a0254bf0a191bd7340b
John Munsch <john.munsch@gmail.com>
2019-02-09 13:19:55 -0600

    Tweaked the nodemon settings to not restart constantly.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const pubsub = new PubSub();
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { pubsub }
+  context: req => ({
+    ...req,
+    pubsub
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
