98f4861924dd506848aaf6f1cb53d398de5cafa5
John Munsch <john.munsch@gmail.com>
2019-02-09 20:37:30 -0600

    Filled in a lot of signin/register page.

--- app/js/sign-in-or-register-page.component.js
+++ app/js/sign-in-or-register-page.component.js
@@ -1,10 +1,94 @@
 import { LitElement, html } from 'lit-element';
 
 class SignInOrRegisterPage extends LitElement {
+  static get properties() {
+    return {
+      email: { type: String },
+      password: { type: String },
+      login: { type: Boolean }
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.email = '';
+    this.password = '';
+
+    this.login = true;
+  }
+
   createRenderRoot() {
     // This avoids us building our component with Shadow DOM so we can use Bootstrap
     // or another CSS framework (which Shadow DOM would interfere with).
     return this;
+  }
+
+  register() {
+    console.log('register', this.email, this.password);
+  }
+
+  signin() {
+    console.log('signin', this.email, this.password);
+  }
+
+  switch() {
+    this.login = !this.login;
+  }
+
+  emailChange(e) {
+    this.email = e.target.value;
+  }
+
+  passwordChange(e) {
+    this.password = e.target.value;
+  }
+
+  form(login) {
+    if (login) {
+      return html`
+        <form class="login-form">
+          <input
+            type="text"
+            placeholder="Email Address"
+            @input="${e => this.emailChange(e)}"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            @input="${e => this.passwordChange(e)}"
+          />
+          <button @click="${this.signin}">
+            Sign In
+          </button>
+          <p class="message">
+            Not registered?
+            <a @click="${this.switch}">Register an account</a>
+          </p>
+        </form>
+      `;
+    } else {
+      return html`
+        <form class="register-form">
+          <input
+            type="text"
+            placeholder="Email Address"
+            @input="${e => this.emailChange(e)}"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            @input="${e => this.passwordChange(e)}"
+          />
+          <button @click="${this.register}">
+            Register
+          </button>
+          <p class="message">
+            Already registered? <a @click="${this.switch}">Sign In</a>
+          </p>
+        </form>
+      `;
+    }
   }
 
   render() {
@@ -17,25 +101,7 @@ class SignInOrRegisterPage extends LitElement {
         </div>
         <div class="form">
           <div class="thumbnail"><img class="logo" src="img/meeple.svg" /></div>
-          <form class="register-form">
-            <input type="text" placeholder="Email Address" />
-            <input type="password" placeholder="Password" />
-            <button>
-              Register
-            </button>
-            <p class="message">Already registered? <a>Sign In</a></p>
-          </form>
-          <form class="login-form">
-            <input type="text" placeholder="Email Address" />
-            <input type="password" placeholder="Password" />
-            <button>
-              Sign In
-            </button>
-            <p class="message">
-              Not registered?
-              <a>Register an account</a>
-            </p>
-          </form>
+          ${this.form(this.login)}
         </div>
       </div>
     `;
